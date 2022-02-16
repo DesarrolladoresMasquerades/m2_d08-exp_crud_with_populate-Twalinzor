2c45619b1eeb9f892a2edb46f1bbe20a746b0d67
Stan T <86654387+Twalinzor@users.noreply.github.com>
2022-02-16 17:00:24 +0100

    .

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -22,19 +22,51 @@ router.get("/post-create", (req, res) => {
 
 // <form action="/post-create" method="POST">
 
-// ... your code here
+router.post("/post-create", (req, res)=>{
+  const title = req.body.title
+  const content = req.body.content
+  const author = req.body.author
+  //const {title, content, author} = req.body
+  //this is the short syntaz for the lines above
+  //it is called "destructuring"
+
+  Post.create({title, content, author})
+    .then(newPost=>{
+      User.findByIdAndUpdate(author, {$push: {posts: newPost._id}})
+    })
+    .then(()=>res.redirect("/posts"))
+    .catch(error=>console.log(error))
+})
 
 // ****************************************************************************************
 // GET route to display all the posts
 // ****************************************************************************************
 
-// ... your code here
+router.get("/posts", (req, res)=>{
+  Post.find()
+    .populate("author")
+    .then(posts=>res.render("posts/list", {posts}))
+})
 
 // ****************************************************************************************
 // GET route for displaying the post details page
 // shows how to deep populate (populate the populated field)
 // ****************************************************************************************
 
-// ... your code here
+router.get("/posts/:id", () => {
+  Post.findById(req.params.id)
+    .populate("author")
+    .populate("comments")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "author", // author of the COMMENT
+        model: "User",
+      },
+    })
+    .then((post) => {
+      res.render("posts/details", post);
+    });
+});
 
 module.exports = router;
